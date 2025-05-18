be376eb501a3654f2a5a0ecca91f44e6b2c5fae4
dawgdevv <nraj02415@gmail.com>
2025-05-18 14:31:30 +0530

    feat: configure CORS middleware with specific options for API requests

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
